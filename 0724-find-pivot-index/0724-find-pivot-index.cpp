80b34e88d80fb85e48dbf93acb7af07ee1315047
muskan308 <73331884+muskan308@users.noreply.github.com>
2023-01-19 12:00:28 +0530

    Time: 23 ms (88.16%), Space: 31.1 MB (57.61%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -3,27 +3,21 @@ public:
     int pivotIndex(vector<int>& nums) {
         int size = nums.size();
         if(size == 1) return 0;
-        vector<int> lrprefix(size), rlprefix(size);
         
-        lrprefix[0] = nums[0];
-        rlprefix[size-1] = nums[size-1];
-        
-        for(int i = 1; i < size; i++){
-            lrprefix[i] = lrprefix[i-1] + nums[i];
-            // cout << lrprefix[i] << " ";
+        int Sum = 0, preSum =0;
+        for(auto x: nums){
+            Sum += x;
         }
-        
-        for(int i = size-2 ; i >=0 ;i--){
-            rlprefix[i] = rlprefix[i+1] + nums[i];
-            // cout << rlprefix[i] << " ";
-        }
-        
-        if(rlprefix[1] == 0) return 0;
        
-        for(int i = 1; i < size-1; i++){
-            if(lrprefix[i-1] == rlprefix[i+1]) return i;
+        
+        for(int i = 0; i < size; i++){
+            
+           if(Sum - preSum - nums[i] == preSum ){
+               return i;
+           }
+            preSum += nums[i];
         }
-         if(lrprefix[size-2] == 0) return size-1;
+         
         return -1;
         
     }
