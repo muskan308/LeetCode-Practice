563c83b8b81bf8c062115689cedcdf0a771a1586
muskan308 <73331884+muskan308@users.noreply.github.com>
2023-01-29 11:14:28 +0530

    Time: 3 ms (70.04%), Space: 6 MB (7.16%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long long s=0, e=x, ans, mid;   //long long due to some of test cases overflows integer limit.
-        while(s<=e){             
-            mid=(s+e)/2;
-            if(mid*mid==x) return mid;     //if the 'mid' value ever gives the result, we simply return it.
-            else if(mid*mid<x){             
-                s=mid+1;        //if 'mid' value encounterted gives lower result, we simply discard all the values lower than mid.
-                ans=mid;        //an extra pointer 'ans' is maintained to keep track of only lowest 'mid' value. 
+        long long low = 1 , high = x;
+        long ans;
+        while(low <= high){
+            long mid = low+(high-low)/2;
+            if(mid*mid == x) return mid;
+            else if(mid*mid < x) {
+                ans = mid;
+                low = mid+1;
+            }else{
+                high = mid-1;
             }
-            else e=mid-1;       //if 'mid' value encountered gives greater result, we simply discard all the values greater than mid. 
         }
-        return ans;  
+        return ans;
     }
 };
