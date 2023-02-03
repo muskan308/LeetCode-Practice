bfb63266d99cc3c24f4fdf0c02c1d03492586b0f
muskan308 <73331884+muskan308@users.noreply.github.com>
2023-02-04 01:06:11 +0530

    Time: 4 ms (37.91%), Space: 6.5 MB (11.69%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> st;
+        
+       for(int i = 0; i < s.size() ; i++){
+            if(s[i] == '(' || s[i]=='[' || s[i] == '{'){
+            st.push(s[i]);
+        } else if(st.empty() && (s[i] ==']'||s[i]=='}'||s[i]==')')) return false;
+           else if(!st.empty() && (st.top() == '(' && s[i] != ')') || (st.top() == '[' && s[i] != ']') ||(st.top() == '{' && s[i] != '}')) return false;
+       
+           else {
+            st.pop();
+        }
+       }
+            return st.empty();
+    }
+};
