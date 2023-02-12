6743333fba50f61910841b441dd3676f93768596
muskan308 <73331884+muskan308@users.noreply.github.com>
2023-02-12 19:05:22 +0530

    Time: 7 ms (89.51%), Space: 15 MB (8.52%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> dsf(vector<vector<int>> &image, int sr, int sc, int newcolor,int color,vector<vector<bool>> &visited){
-        visited[sr][sc] = true;
-        if(sr-1 >= 0 && visited[sr-1][sc] == false && image[sr-1][sc] == color){
-            dsf(image, sr-1, sc, newcolor, color, visited);
-        }
-        if(sc-1 >=0 && visited[sr][sc-1] == false && image[sr][sc-1] == color){
-            dsf(image, sr, sc-1, newcolor, color, visited);
-        }
-        if(sc+1 < image[0].size() && visited[sr][sc+1] == false && image[sr][sc+1] == color){
-            dsf(image, sr, sc+1, newcolor, color, visited);
-        }
-        if(sr+1 < image.size() && visited[sr+1][sc] == false && image[sr+1][sc] == color){
-            dsf(image, sr+1, sc, newcolor, color, visited);
-        }
+    vector<vector<int>> dsf(vector<vector<int>> &image, int sr, int sc, int newcolor,int color){
+        // if(image[sr][sc] == newcolor) return image;
+        
         image[sr][sc] = newcolor;
+        
+        if(sr-1 >= 0 && image[sr-1][sc] == color){ 
+            dsf(image, sr-1, sc, newcolor, color);
+        }
+        if(sc-1 >=0 && image[sr][sc-1] == color){
+            dsf(image, sr, sc-1, newcolor, color);
+        }
+        if(sc+1 < image[0].size() && image[sr][sc+1] == color){
+            dsf(image, sr, sc+1, newcolor, color);
+        }
+        if(sr+1 < image.size() && image[sr+1][sc] == color){
+            dsf(image, sr+1, sc, newcolor, color);
+        }
+        
         return image;
     }
     
@@ -22,10 +25,9 @@ public:
         
         if(color == image[sr][sc]) return image;
         else{
-            int rows= image.size();
-            int cols = image[0].size();
-            vector<vector<bool>> visited(rows, vector<bool>(cols, false));
-            image = dsf(image, sr, sc, color, image[sr][sc], visited);
+            
+            //vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+            image = dsf(image, sr, sc, color, image[sr][sc]);
         }
         return image;
     }
