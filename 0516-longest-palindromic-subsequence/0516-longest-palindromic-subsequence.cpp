56545aa6b65dc3c4c20606141748b4f874bf58bd
muskan308 <73331884+muskan308@users.noreply.github.com>
2023-02-18 21:36:44 +0530

    Time: 38 ms (99.78%), Space: 10.6 MB (86.86%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     
-  int longestPalindromeSubseq(string s) {
-        int n = s.size();
-        vector<int> v0(n), v1(n,1), v(n), *i_2=&v0, *i_1=&v1, *i_=&v;
-        for(int i=2;i<=n;i++) {//length
-            for(int j=0;j<n-i+1;j++)//start index
-                i_->at(j) = s[j]==s[i+j-1]?2+i_2->at(j+1):max(i_1->at(j),i_1->at(j+1));
-            swap(i_1,i_2);    
-            swap(i_1,i_); //rotate i_2, i_1, i_
-        }
-        return i_1->at(0); 
+     int memo[1000][1000] = {};
+    int longestPalindromeSubseq(const string& s) {
+        return dp(s, 0, s.size() - 1);
     }
- 
+    int dp(const string& s, int l, int r) {
+        if (l > r) return 0; // Return 0 since it's empty string
+        if (l == r) return 1; // Return 1 since it has 1 character
+        if (memo[l][r] != 0) return memo[l][r];
+        if (s[l] == s[r])
+            return memo[l][r] = dp(s, l+1, r-1) + 2;
+        return memo[l][r] = max(dp(s, l+1, r), dp(s, l, r-1));
+    }
 };
