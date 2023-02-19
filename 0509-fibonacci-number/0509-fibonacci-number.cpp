aa656bfb0e5d5a57a18d5e5152058dcb2e077c72
muskan308 <73331884+muskan308@users.noreply.github.com>
2023-02-19 23:11:06 +0530

    Time: 0 ms (100.00%), Space: 6 MB (69.26%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
-    int mem[31] = {};
+    // int mem[31] = {};
     int fib(int n) {
-        if(n == 0 || n == 1)return n;
-        if(mem[n]) return mem[n];
-        return mem[n] = fib(n-1)+fib(n-2);
+//         if(n == 0 || n == 1)return n;
+//         if(mem[n]) return mem[n];
+//         return mem[n] = fib(n-1)+fib(n-2);
+        if(n == 0 || n ==1)return n;
+        vector<int> mem(n+1, -1);
+        mem[0] = 0;
+        mem[1] = 1;
+        
+        for(int i = 2; i <=n ; i++){
+            mem[i] = mem[i-1]+mem[i-2];
+        }
+        return mem[n];
     }
 };
