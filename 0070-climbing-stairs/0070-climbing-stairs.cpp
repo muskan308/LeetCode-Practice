171f78815c22832ee258d66e3d3415aa01c94105
muskan308 <73331884+muskan308@users.noreply.github.com>
2023-02-19 23:58:59 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (18.81%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int ans(int n, vector<int> &mem){
-        if(n == 0)return 1;
-        if(n < 0) return 0;
+        if(n == 0 || n==1)return 1;
+        // if(n ==1) return 1;
         if(mem[n] != -1)return mem[n];
         return mem[n] = ans(n-1, mem) + ans(n-2, mem);
     }
