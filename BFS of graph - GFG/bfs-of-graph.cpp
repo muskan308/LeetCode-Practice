c5574ee61515f99d44ba4b1d275f5b3c63ec0264
muskan308 <73331884+muskan308@users.noreply.github.com>
2023-02-23 14:31:16 +0530

    Added solution - LeetHub

--- BFS of graph - GFG/bfs-of-graph.cpp
+++ BFS of graph - GFG/bfs-of-graph.cpp
@@ -6,29 +6,35 @@ using namespace std;
 class Solution {
   public:
     // Function to return Breadth First Traversal of given graph.
-    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
-        // Code here
-        vector<int> ans;
-        unordered_set<int> visited;
-        int source = 0;
+    
+    void bsf(vector<int> adj[], int source, vector<int> &ans, unordered_set<int> &visited){
+        queue<int> q;
+        q.push(source);
         visited.insert(source);
-        ans.push_back(source);
         
-        queue<int> bsf;
-        bsf.push(source);
-        
-        while(!bsf.empty()){
-            for(int i = 0;i < adj[source].size() ; i++){
-                if(!(visited.count(adj[source][i]))){
+        while(!q.empty()){
+            for(int i = 0; i < adj[source].size() ; i++){
+                if(visited.count(adj[source][i]) == false){
+                    q.push(adj[source][i]);
                     visited.insert(adj[source][i]);
-                    ans.push_back(adj[source][i]);
-                    bsf.push(adj[source][i]);
                 }
             }
-            bsf.pop();
-            source = bsf.front();
+            ans.push_back(source);
+            q.pop();
+            source = q.front();
         }
-           return ans; 
+    }
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        // Code here
+        unordered_set<int> visited;
+        vector<int> ans;
+        // for(int i = 0; i < V ; i++){
+        //     if(visited.count(i) == false){
+        //         bsf(adj, i, ans, visited);
+        //     }
+        // }
+        bsf(adj, 0, ans, visited);
+        return ans;
     }
 };
 
