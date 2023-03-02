b1e8259846c67b0cf573e93efbf8bbf9d791903b
muskan308 <73331884+muskan308@users.noreply.github.com>
2023-03-03 00:18:26 +0530

    Added solution - LeetHub

--- Sum of Middle Elements of two sorted arrays - GFG/sum-of-middle-elements-of-two-sorted-arrays.cpp
+++ Sum of Middle Elements of two sorted arrays - GFG/sum-of-middle-elements-of-two-sorted-arrays.cpp
@@ -11,32 +11,34 @@ using namespace std;
 class Solution {
 public:
     int findMidSum(int ar1[], int ar2[], int n) {
-            if(n==1) return ar1[0] + ar2[0];
-  if(n == 2) return max(ar1[0], ar2[0]) + min(ar1[1], ar2[1]);
-  
-  int i = 0, j = 0;
-  int k = 0;
-  int merged[2*n];
-  
-  while(i < n && j < n){
-    if(ar1[i] <= ar2[j]){
-      merged[k] = ar1[i];
-      i++;k++;
-    }else{
-      merged[k] = ar2[j];
-      j++;k++;
-    }
-  }
-  while(i < n){
-    merged[k] = ar1[i];
-    i++;k++;
-  }
-  while(j < n){
-    merged[k] = ar2[j];
-    j++;k++;
-  }
-  
-  return merged[n] + merged[n-1];
+            if(n==1){
+                return ar1[0]+ar2[0];
+            }
+            if(n == 2){
+                return max(ar1[0], ar2[0]) + min(ar1[1], ar2[1]);
+            }
+            // code here 
+            int low = 0, high = n-1;
+            int ans;
+            while(low <= high) {
+                int cut1 = low +(high-low)/2;
+                int cut2 = n - cut1;
+                
+                int l1 = (cut1 == 0 ? INT_MIN : ar1[cut1-1]);
+                int l2 = (cut2 == 0 ? INT_MIN : ar2[cut2-1]);
+                int r1 = (cut1 == n ? INT_MAX : ar1[cut1]);
+                int r2 = (cut2 == n ? INT_MAX : ar2[cut2]);
+                
+                if(l1 > r2){
+                    high = cut1-1;
+                }else if(l2 > r1) {
+                    low = cut1+1;
+                }else{
+                    ans = max(l1,l2) + min(r1,r2);
+                    break;
+                }
+            }
+            return ans;
     }
 };
 
