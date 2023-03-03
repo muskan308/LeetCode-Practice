e423e2b1977526b4d3c0a09213e5b6980ac92796
muskan308 <73331884+muskan308@users.noreply.github.com>
2023-03-04 00:53:25 +0530

    Time: 47 ms (26.86%), Space: 26.1 MB (60.00%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,28 +11,21 @@
  */
 class Solution {
 public:
-    int preIndex;
-    map<int, int> mp;
-    TreeNode *solve(vector<int> pre, int startIn, int endIn){
-        if(startIn > endIn){
-            return NULL;
-        }
-        int element = pre[preIndex++];
-        TreeNode *root = new TreeNode(element);
-        int pos = mp[element];
-        root->left = solve(pre, startIn, pos-1);
-        root->right = solve(pre, pos+1, endIn);
-        return root;
+    
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int rootIdx = 0;
+        return build(preorder, inorder, rootIdx, 0, inorder.size()-1);
     }
     
-    TreeNode* buildTree(vector<int>& pre, vector<int>& in) {
-        int n= in.size();
+    TreeNode* build(vector<int>& preorder, vector<int>& inorder, int& rootIdx, int left, int right) {
+        if (left > right) return NULL;
+        int pivot = left;  // find the root from inorder
+        while(inorder[pivot] != preorder[rootIdx]) pivot++;
         
-        for(int i = 0; i < n ; i++){
-            mp[in[i]] = i;
-        }
-        preIndex = 0;
-        TreeNode * ans = solve(pre, 0, n-1);
-        return ans;
+        rootIdx++;
+        TreeNode* newNode = new TreeNode(inorder[pivot]);
+        newNode->left = build(preorder, inorder, rootIdx, left, pivot-1);
+        newNode->right = build(preorder, inorder, rootIdx, pivot+1, right);
+        return newNode;
     }
 };
