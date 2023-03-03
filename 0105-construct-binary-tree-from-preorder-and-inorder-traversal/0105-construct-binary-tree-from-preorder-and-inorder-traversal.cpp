c27d0acf5d9a3690c27ebf087fa42c64fdde0d75
muskan308 <73331884+muskan308@users.noreply.github.com>
2023-03-04 00:56:24 +0530

    Time: 22 ms (66.60%), Space: 26.3 MB (58.17%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -12,21 +12,21 @@
 class Solution {
 public:
     int preInd=0;
+    map<int, int> mp;
 TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+    for(int i = 0; i < inorder.size() ; i++){
+        mp[inorder[i]] = i;
+    }
     return createTree(preorder,inorder,0,inorder.size() - 1);
 }
 TreeNode* createTree(vector<int>& preorder, vector<int>& inorder,int start, int end){
     if(start > end){
         return NULL;
     }
-    TreeNode* node=new TreeNode(preorder[preInd++]);
-    int pos;
-    for(int i=start;i<=end;i++){
-        if(inorder[i]==node->val){
-            pos=i;
-            break;
-        }
-    }
+    int ele = preorder[preInd++];
+    TreeNode* node=new TreeNode(ele);
+    int pos = mp[ele];
+    
     node->left =createTree(preorder, inorder,start,pos-1);
     node->right =createTree(preorder, inorder, pos+1,end);
     return node;
