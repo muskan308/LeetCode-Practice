e8e5957471ed8e7559bf5077cd770713703a835d
muskan308 <73331884+muskan308@users.noreply.github.com>
2023-03-03 14:10:56 +0530

    Time: 16 ms (50.97%), Space: 7.9 MB (53.33%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> roman;
-        roman['I'] = 1;
-        roman['V'] = 5;
-        roman['X'] = 10;
-        roman['L'] = 50;
-        roman['C'] = 100;
-        roman['D'] = 500;
-        roman['M'] = 1000;
-        
-        int ans = 0;
-        for(int i = 0; i < s.size() ;){
-            if(roman[s[i+1]] > roman[s[i]]){
-                ans += (roman[s[i+1]] - roman[s[i]]);
-                i = i+2;
-            }
-            else{
-                 ans+= roman[s[i]];
-                i = i+1;
-            }
-          
-        }return ans;
+        unordered_map<char, int> T = { { 'I' , 1 },
+                                   { 'V' , 5 },
+                                   { 'X' , 10 },
+                                   { 'L' , 50 },
+                                   { 'C' , 100 },
+                                   { 'D' , 500 },
+                                   { 'M' , 1000 } };
+                                   
+   int sum = T[s.back()];
+   for (int i = s.length() - 2; i >= 0; --i) 
+   {
+       if (T[s[i]] < T[s[i + 1]])
+       {
+           sum -= T[s[i]];
+       }
+       else
+       {
+           sum += T[s[i]];
+       }
+   }
+   
+   return sum;
     }
 };
