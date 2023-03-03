3e80137ad81a4c65bb9f6df6216b31250ab258b5
muskan308 <73331884+muskan308@users.noreply.github.com>
2023-03-04 00:57:08 +0530

    Time: 16 ms (84.81%), Space: 26.5 MB (32.44%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -17,9 +17,9 @@ TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
     for(int i = 0; i < inorder.size() ; i++){
         mp[inorder[i]] = i;
     }
-    return createTree(preorder,inorder,0,inorder.size() - 1);
+    return createTree(preorder,0,inorder.size() - 1);
 }
-TreeNode* createTree(vector<int>& preorder, vector<int>& inorder,int start, int end){
+TreeNode* createTree(vector<int>& preorder,int start, int end){
     if(start > end){
         return NULL;
     }
@@ -27,8 +27,8 @@ TreeNode* createTree(vector<int>& preorder, vector<int>& inorder,int start, int 
     TreeNode* node=new TreeNode(ele);
     int pos = mp[ele];
     
-    node->left =createTree(preorder, inorder,start,pos-1);
-    node->right =createTree(preorder, inorder, pos+1,end);
+    node->left =createTree(preorder,start,pos-1);
+    node->right =createTree(preorder, pos+1,end);
     return node;
 }
 };
