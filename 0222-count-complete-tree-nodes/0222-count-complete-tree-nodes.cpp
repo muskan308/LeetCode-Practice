440f7e8ce3da2b5a21cc8c3e554dc2ac61ae1e5c
muskan308 <73331884+muskan308@users.noreply.github.com>
2023-03-11 23:59:34 +0530

    Time: 36 ms (61.18%), Space: 30.8 MB (94.84%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -11,8 +11,29 @@
  */
 class Solution {
 public:
+    
+    int height(TreeNode* root){
+        if(!root) return 0;
+        int lh = height(root->left);
+        int rh = height(root->right);
+        return 1 + max(lh, rh);
+    }
+   
+    int rheight(TreeNode* root){
+        if(root==NULL)return 0;
+        return 1 + rheight(root->right);
+    }
+     int lheight(TreeNode* root){
+        if(root==NULL)return 0;
+        return 1 + lheight(root->left);
+    }
     int countNodes(TreeNode* root) {
-        if(root == NULL) return 0;
+        if(root==NULL) return 0;
+        if(lheight(root) == rheight(root)){
+            int h = height(root);
+            return pow(2,h)-1;
+        }
+        
         return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };
