eddceec9b2d762600ef0616921235a18c91ce572
Muskan Thapa <73331884+muskan308@users.noreply.github.com>
2023-03-23 01:45:55 +0530

    Time: 10 ms (41.89%), Space: 6.8 MB (69.50%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -12,28 +12,14 @@ public:
         // }
         string str ="";
         int carry =0;
-        while(i >= 0 && j >= 0){
-            int a = num1[i]-'0', b = num2[j]-'0';
+        while(i >= 0 || j >= 0 || carry){
+            int a = (i>=0 ? num1[i]-'0' : 0), b = (j >=0 ? num2[j]-'0' :0);
             i--;j--;
             int val = a+b+carry;
             carry = val/10;
             str+= to_string(val%10);
         }
-        while(i >= 0){
-            int val = num1[i]-'0'+carry;
-            str+= to_string(val%10);
-            carry = val/10;
-            i--;
-        }
-    
-       
-        while(j >= 0){
-            int val = num2[j]-'0'+carry;
-            str+= to_string(val%10);
-            carry = val/10;
-            j--;
-        }
-        if(carry!=0) str+= to_string(carry);
+        
         for(int k = 0; k < str.size()/2 ;k++){
             swap(str[k], str[str.size()-1-k]);
         }
