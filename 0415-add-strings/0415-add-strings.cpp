b5f3c0faad2ad10103bbfda661dd94165dc95fe9
Muskan Thapa <73331884+muskan308@users.noreply.github.com>
2023-03-23 01:39:24 +0530

    Time: 8 ms (43.58%), Space: 7.1 MB (31.43%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        stack<int> st1, st2;
+        //stack<int> st1, st2;
         
-        for(int i = 0; i < num1.size() ; i++){
-            st1.push(num1[i]-'0');
-        }
-        for(int i = 0; i < num2.size(); i++){
-            st2.push(num2[i]-'0');
-        }
-        int carry =0;
-        
+        int i = num1.size()-1, j = num2.size()-1;
+        // for(int i = 0; i < num1.size() ; i++){
+        //     st1.push(num1[i]-'0');
+        // }
+        // for(int i = 0; i < num2.size(); i++){
+        //     st2.push(num2[i]-'0');
+        // }
         stack<int> ans;
-        while(!st1.empty() && !st2.empty()){
-            int a = st1.top(), b = st2.top();
-            st1.pop(); st2.pop();
+        int carry =0;
+        while(i >= 0 && j >= 0){
+            int a = num1[i]-'0', b = num2[j]-'0';
+            i--;j--;
             int val = a+b+carry;
             carry = val/10;
             ans.push(val%10);
         }
-        while(!st1.empty()){
-            int val = st1.top()+carry;
+        while(i >= 0){
+            int val = num1[i]-'0'+carry;
             ans.push(val%10);
             carry = val/10;
-            st1.pop();
+            i--;
         }
-        while(!st2.empty()){
-            int val = st2.top()+carry;
+    
+       
+        while(j >= 0){
+            int val = num2[j]-'0'+carry;
             ans.push(val%10);
             carry = val/10;
-            st2.pop();
+            j--;
         }
         if(carry!=0) ans.push(carry);
         string str ="";
