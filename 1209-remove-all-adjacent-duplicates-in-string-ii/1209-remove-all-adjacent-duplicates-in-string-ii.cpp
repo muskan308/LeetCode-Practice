6c212e31aeb3022321d9d73f2b210a4843557412
Muskan Thapa <73331884+muskan308@users.noreply.github.com>
2023-03-28 01:08:10 +0530

    Time: 37 ms (36.12%), Space: 10.1 MB (83.33%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string removeDuplicates(string s, int k) {
         if(s.size() == 0) return "";
-        stack<pair<char, int>> str;
+        deque<pair<char, int>> str;
         
         
         // str.push({s[0], 1});
@@ -12,33 +12,34 @@ public:
         while(i < n){
             //freq[s[i]] = freq[s[i]] ? freq[s[i]] : 1;
             if(str.empty()){
-                str.push({s[i], 1});
+                str.push_back({s[i], 1});
             }else{
-                if(str.top().first != s[i]){
-                str.push({s[i], 1});
+                if(str.back().first != s[i]){
+                str.push_back({s[i], 1});
             }
             else{
-               str.top().second += 1;
-                if(str.top().second == k){
-                    str.pop();
+               str.back().second += 1;
+                if(str.back().second == k){
+                    str.pop_back();
                 }
                }
             }
             i++;
         }
         string st = "";
-        stack<char> st1;
+        // stack<char> st1;
         while(!str.empty()){
-            while(str.top().second != 0){
-                st1.push(str.top().first);
-                str.top().second--;
+            while(str.front().second != 0){
+                // st1.push(str.top().first);
+                st += str.front().first;
+                str.front().second--;
             }
-            str.pop();
+            str.pop_front();
         }
-        while(!st1.empty()){
-            st += st1.top();
-            st1.pop();
-        }
+        // while(!st1.empty()){
+        //     st += st1.top();
+        //     st1.pop();
+        // }
         return st;
     }
 };
